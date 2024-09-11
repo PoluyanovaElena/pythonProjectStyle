5df3d940b811d890c298f1779da0e9ec822d2c03
PoluyanovaElena <poluyanovaeo@gmail.com>
2024-09-11 11:35:04 +0500

    задача 39

---
+++ уроки программирования/Homework39.py
@@ -0,0 +1,39 @@
+from threading import Thread, Lock
+from random import randint
+from time import sleep
+class Bank:
+    def __init__(self, balance):
+        self.balance = balance
+        self.lock = Lock()
+
+    def deposit(self):
+        for i in range(100):
+            random_number = randint(50, 500)
+            self.balance += random_number
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            print(f'Пополнение: {random_number}. Баланс: {self.balance}')
+            sleep(0.001)
+
+    def take(self):
+        for i in range(100):
+            random_number = randint(50, 500)
+            print(f'Запрос на {random_number}')
+            if random_number <= self.balance:
+                self.balance -= random_number
+                print(f'Снятие: {random_number}. Баланс: {self.balance}')
+            else:
+                print("Запрос отклонён, недостаточно средств")
+                self.lock.acquire()
+
+bk = Bank(0)
+
+th1 = Thread(target=Bank.deposit, args=(bk,))
+th2 = Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
