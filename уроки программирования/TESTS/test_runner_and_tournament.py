3fda82684406e975680b54bc9834ea8f90dd30d3
PoluyanovaElena <poluyanovaeo@gmail.com>
2024-09-24 11:49:34 +0500

    задача m12

---
+++ уроки программирования/TESTS/test_runner_and_tournament.py
@@ -0,0 +1,48 @@
+import unittest
+from runner_and_tournament import Runner, Tournament
+
+
+class TournamentTest(unittest.TestCase):
+    is_frozen = True
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+    # @unittest.skip('Тесты в этом кейсе заморожены')
+    def setUp(self):
+        self.usain = Runner("Усэйн", speed=10)
+        self.andrey = Runner("Андрей", speed=9)
+        self.nick = Runner("Ник", speed=3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for result in cls.all_results.values():
+            print(result)
+
+    def test_usain_and_nick(self):
+        tournament = Tournament(90, self.usain, self.nick)
+        result = tournament.start()
+        # Сохраняем имена бегунов
+        formatted_result = {place: runner.name for place, runner in result.items()}
+        TournamentTest.all_results['test_usain_and_nick'] = formatted_result
+        self.assertTrue(result[max(result.keys())].name == "Ник")
+
+    def test_andrey_and_nick(self):
+        tournament = Tournament(90, self.andrey, self.nick)
+        result = tournament.start()
+        # Сохраняем имена бегунов
+        formatted_result = {place: runner.name for place, runner in result.items()}
+        TournamentTest.all_results['test_andrey_and_nick'] = formatted_result
+        self.assertTrue(result[max(result.keys())].name == "Ник")
+
+    def test_usain_andrey_and_nick(self):
+        tournament = Tournament(90, self.usain, self.andrey, self.nick)
+        result = tournament.start()
+        # Сохраняем имена бегунов
+        formatted_result = {place: runner.name for place, runner in result.items()}
+        TournamentTest.all_results['test_usain_andrey_and_nick'] = formatted_result
+        self.assertTrue(result[max(result.keys())].name == "Ник")
+
+
+if __name__ == '__main__':
+    unittest.main(argv=[''], verbosity=2, exit=False)
